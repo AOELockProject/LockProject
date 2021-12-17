759db7b072af84c87f34ab593362f24bd6866280
Wk-P <72795540+Wk-P@users.noreply.github.com>
2021-12-18 07:56:51 +0900

    Add files via upload

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,5 @@
 #include <libraries.h>
 
-// RFID 读取显示
-
 void setup()
 {
   Serial.begin(9600);
@@ -9,6 +7,7 @@ void setup()
   Buzzer_Init(noteDuration, BuzzerPin);
   RFID_Init();
   RGB_Init();
+  fingerPrint_Init();
 }
 
 // 默认读卡模式
@@ -34,7 +33,8 @@ void loop()
         piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
         piccType != MFRC522::PICC_TYPE_MIFARE_4K)
     {
-      Serial.println("不支持读取此卡类型");
+      lcd.setCursor(0, 0);
+      lcd.print("Error");
       return;
     }
     // 将NUID保存到nuidPICC数组
@@ -42,13 +42,6 @@ void loop()
     {
       nuidPICC[i] = rfid.uid.uidByte[i];
     }
-    Serial.print("十六进制UID:");
-    printHex(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-
-    Serial.print("十进制UID:");
-    printDec(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
 
     // 使放置在读卡区的IC卡进入休眠状态，不再重复读卡
     rfid.PICC_HaltA();
@@ -57,16 +50,10 @@ void loop()
     rfid.PCD_StopCrypto1();
     if (nuidPICC[0] == 0x33 && nuidPICC[1] == 0x10 && nuidPICC[2] == 0x52 && nuidPICC[3] == 0x97)
     {
-      Serial.println("身份确认解锁成功!");
-      RGB_color_green();
-      buzzerSpeaker(noteDuration, BuzzerPin, 1);
-      servo_turn();
-      delay(5000);
-      servo_turn();
+      passwordRight();
     }
     else
     {
-      Serial.println("非系统用户，无法解锁!");
       passwordWrong();
     }
     delay(1000);
@@ -84,18 +71,59 @@ void loop()
     if (!inputPasswordCompared(In_buffer, Password))
     {
       passwordRight();
-    } else {
+    }
+    else
+    {
       passwordWrong();
     }
   }
 
   case sFingerPrint:
   {
-    /*指纹识别模式*/
+    /* 指纹识别模式 */
+    /* Default mode is Search */
+    // LCD display
+    lcd.setCursor(0, 0);
+    lcd.print("Fingerprint");
+    delay(2000);
+    lcd.setCursor(0, 0);
+    lcd.print("1.Open 2.Change");
+    lcd.setCursor(0, 1);
+    lcd.print("3.Delete");
 
+    char choose = keyRead();
 
+    switch (choose) {
+      case '1':{
+        finger.getTemplateCount();
+        Serial.print(finger.templateCount);
+        if (getFingerprintIDez() == -1)
+        {
+          lcd.setCursor(0, 0);
+          lcd.print("Wrong fingrprint");
+        } else {
+          lcd.setCursor(0, 0);
+          lcd.print("Open!");
+          servo_turn();
+        }
+        delay(50);
+        break;
+      }
+      case '2': {
+        while ((id = readnumber()) == 0);
+        getFingerprintEnroll();
+        lcd.setCursor(0, 0);
+        lcd.print("Changed/Saved!");
+        break;
+      }
+      case '3': {
+        finger.emptyDatabase();
+        break;
+      }
+      default:{}
+    }
     break;
   }
-  default: {}
+  default:{}
   }
 }

8215542c51c3893e7d421ff6f913d5e70916b11e
jiangwel <84135487+jiangwel@users.noreply.github.com>
2021-12-22 00:26:59 +0900

    Add files via upload

---
+++ Modified version by JW/main_Mega_2560.ino
@@ -0,0 +1,263 @@
+#include <usrlib.h>
+
+SoftwareSerial fingerSerial(18, 19); // 18: mega TX -> s608 RX / 19:mega RX -> s608 TX
+SoftwareSerial unoSerial(16, 17);    // 14: mega TX / 15: RX1
+#define fingerSerial Serial1
+#define unoSerial Serial2
+// fingerprint
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&fingerSerial);
+uint8_t id;
+int LED_pins[] = {35, 36, 37};     // yellow:RFID run, green:Keypad run, red: 
+
+int PassMode = sRFID;
+//char key_pressed=0;//Modified by JW
+
+// keypad listener
+void keypadEvent(KeypadEvent key);
+
+
+int clooock=1;//Modified by JW
+int* cloock = &clooock;//Modified by JW
+
+// Keypad
+const byte ROWS = 4;
+const byte COLS = 3;
+char *enteredPW;//Modified by JW
+
+char keys[ROWS][COLS] = {
+    {'1', '2', '3'},
+    {'4', '5', '6'},
+    {'7', '8', '9'},
+    {'*', '0', '#'}};
+
+byte rowPins[ROWS] = {46, 41, 42, 44};
+byte colPins[COLS] = {45, 47, 43};
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+
+// keypad.addEventListener(keypadEvent); // Add an event listener for this keypad
+
+// Servo
+Servo myservo;
+const int servoPin = 26;
+int pos = 0;
+
+// Speaker
+int noteDuration = 4, BuzzerPin = 25;
+
+// RGB
+const int RedPin = 22, GreenPin = 23, BluePin = 24;
+const int RGB_pins[3] = {RedPin, GreenPin, BluePin}; // R, G, B
+
+// Password
+char Password[4] = "2021";//Modified by JW
+
+// Lock status
+int lockStatus = CLOSED;
+
+// Input buffer
+char Input_buffer[256];
+char *In_buffer = Input_buffer;
+
+// RFID module
+byte nuidPICC[4];
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+// finger mode select
+/* 1 Search
+   2 Enroll
+   3 Delete All 
+   */
+
+void setup()
+{
+  Serial.begin(9600);
+  fingerSerial.begin(57600);
+  unoSerial.begin(19200);
+  servo_Init(servoPin, myservo);
+  Buzzer_Init(noteDuration, BuzzerPin);
+  RFID_Init(rfid);
+  RGB_Init(RGB_pins);
+  fingerPrint_Init(finger);
+  LED_Init(LED_pins);
+  lockStatus = CLOSED;
+}
+
+// 默认读卡模式
+
+void loop()
+{
+  //Serial.println("----Start---\n");
+  PassMode = modeSelect(keypad);
+
+  //key_pressed = keypad.getKey();//Modified by JW
+  
+
+  switch (PassMode)
+  {
+  case sRFID:
+  {
+    // Running LED
+    digitalWrite(LED_pins[0], HIGH);
+    
+    /* uno Serial message send */
+    unoSerial.write('i');
+    /* RFID 输入模式*/
+    /* Test */
+    // 找卡
+    if (!rfid.PICC_IsNewCardPresent())
+      return;
+    // 验证NUID是否可读
+    if (!rfid.PICC_ReadCardSerial())
+      return;
+    MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+    // 检查是否MIFARE卡类型
+    if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+        piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+        piccType != MFRC522::PICC_TYPE_MIFARE_4K)
+    {
+      unoSerial.write('c');
+      return;
+    }
+    // 将NUID保存到nuidPICC数组
+    for (byte i = 0; i < 4; i++)
+    {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+    // 使放置在读卡区的IC卡进入休眠状态，不再重复读卡
+    rfid.PICC_HaltA();
+
+    // 停止读卡模块编码
+    rfid.PCD_StopCrypto1();
+    if (nuidPICC[0] == 0x33 && nuidPICC[1] == 0x10 && nuidPICC[2] == 0x52 && nuidPICC[3] == 0x97)
+    {
+      unoSerial.write('p');
+      lockStatus = servo_turn(pos, myservo, lockStatus);
+      passwordRight(RGB_pins, noteDuration, BuzzerPin);
+      lockStatus = servo_turn(pos, myservo, lockStatus);
+    }
+    else
+    {
+      unoSerial.write('w');
+      passwordWrong(RGB_pins, noteDuration, BuzzerPin);
+    }
+    delay(1000);
+    RGB_Init(RGB_pins);
+    break;
+  }
+
+  case sKeypad:
+  {
+    // Running LED
+    digitalWrite(LED_pins[1], HIGH);
+    /* 键盘输入密码
+      错误导致蜂鸣器蜂鸣，LCD显示"Wrong", RGB亮红灯
+      密码正确LCD显示"PASS!"
+    */
+    //
+    
+    enteredPW= enterPassword(keypad,cloock);
+    
+
+    for(int ii=0;ii<5;ii++){
+      Serial.println();
+      Serial.print("Entered ps is:");
+      Serial.print(*(enteredPW+ii));
+    }
+
+    if (*(enteredPW+1)==Password[0] &&
+        *(enteredPW+2)==Password[1] &&
+        *(enteredPW+3)==Password[2] &&
+        *(enteredPW+4)==Password[3] )//Modified by JW
+    {
+      unoSerial.write('p');
+      lockStatus = servo_turn(pos, myservo, lockStatus);
+      passwordRight(RGB_pins, noteDuration, BuzzerPin);
+      lockStatus = servo_turn(pos, myservo, lockStatus);
+    }
+    else if((enteredPW!='!' && *enteredPW!=Password && *cloock==0)  ){
+      unoSerial.write('w');
+      passwordWrong(RGB_pins, noteDuration, BuzzerPin);
+    }
+  }
+
+  case sFingerPrint:
+  {
+    digitalWrite(LED_pins[2], HIGH);
+    /* 指纹识别模式 */
+    /* Default mode is Search */
+
+    /*test*/
+    
+    char fingerModeChoose = keyRead(keypad);
+
+    switch (fingerModeChoose) {
+      case '1':{
+        finger.getTemplateCount();
+        Serial.print(finger.templateCount);
+        if (getFingerprintIDez(finger, RGB_pins) == -1)
+        {
+          unoSerial.write('w');
+          RGB_color_red(RGB_pins);
+          delay(2000);
+        } else {
+          unoSerial.write('p');
+          lockStatus = servo_turn(pos, myservo, lockStatus);
+          RGB_color_green(RGB_pins);
+          delay(5000);
+          lockStatus = servo_turn(pos, myservo, lockStatus);
+        }
+        delay(500);
+        RGB_Init(RGB_pins);
+        break;
+      }
+      case '2': {
+        while ((id = readnumber(keypad)) == 0);
+        getFingerprintEnroll(finger, id, RGB_pins);
+        break;
+      }
+      case '3': {
+        finger.emptyDatabase();
+        break;
+      }
+      default:{}
+    }
+    break;
+  }
+  default:{}
+  }
+
+  for (int i = 0;i < 3;i++) {
+    digitalWrite(LED_pins[i], LOW);
+  }
+}
+
+/* Password reset with keypad */
+/// keypad listener
+// void keypadEvent(KeypadEvent key)
+// {
+//     switch (keypad.getState())
+//     {
+//     case PRESSED:
+//         Serial.print(key);
+//         if (key == '#')
+//         {
+//             // clear
+//         }
+//         if (key == '*')
+//         {
+//             // confirm
+//         }
+//         break;
+//     case RELEASED:
+//         if (key == '*')
+//         {
+//         }
+//         break;
+//     case HOLD:
+//         if (key == '*')
+//         {
+//         }
+//         break;
+//     }
+// }
---
+++ Modified version by JW/usrlib.cpp
@@ -0,0 +1,495 @@
+#include <usrlib.h>
+
+void RGB_color(int Rpin, int Gpin, int Bpin, int red, int green, int blue)
+{
+    analogWrite(Rpin, red);
+    analogWrite(Gpin, green);
+    analogWrite(Bpin, blue);
+}
+
+void RGB_color_blue(int RGB_pins[3])
+{
+    RGB_color(RGB_pins[0], RGB_pins[1], RGB_pins[2], 0, 0, 255);
+}
+
+void RGB_color_red(int RGB_pins[3])
+{
+    RGB_color(RGB_pins[0], RGB_pins[1], RGB_pins[2], 255, 0, 0);
+}
+
+void RGB_color_green(int RGB_pins[3])
+{
+    RGB_color(RGB_pins[0], RGB_pins[1], RGB_pins[2], 0, 255, 0);
+}
+
+
+
+// setup 初始化
+/* LCD, servo, RGB, keypad, Buzzer */
+
+
+
+void servo_Init(const int servoPin, Servo myservo)
+{
+    myservo.attach(servoPin);
+}
+
+void RGB_Init(int RGB_pins[3])
+{
+    for (int i = 0; i < sizeof(RGB_pins) / sizeof(const int); i++)
+    {
+        pinMode(RGB_pins[i], OUTPUT);
+    }
+    RGB_color(RGB_pins[0], RGB_pins[1], RGB_pins[2], 0, 0, 0);
+}
+
+void Buzzer_Init(int BuzzerPin, int noteDuration)
+{
+    pinMode(BuzzerPin, OUTPUT);
+    noteDuration = 4;
+}
+
+void RFID_Init(MFRC522 rfid)
+{
+    SPI.begin();
+    rfid.PCD_Init();
+}
+
+void LED_Init(int LED_pins[])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        pinMode(LED_pins[i], OUTPUT);
+    }
+}
+
+// RFID function
+
+void printHex(byte *buffer, byte bufferSize)
+{
+    for (byte i = 0; i < bufferSize; i++)
+    {
+        Serial.print(buffer[i] < 0x10 ? " 0" : "");
+        Serial.print(buffer[i], HEX);
+    }
+}
+
+void printDec(byte *buffer, byte bufferSize)
+{
+    for (byte i = 0; i < bufferSize; i++)
+    {
+        Serial.print(buffer[i] < 0x10 ? " 0" : "");
+        Serial.print(buffer[i], DEC);
+    }
+}
+
+//键盘单个字符读入
+char keyRead(Keypad keypad)
+{
+    return keypad.getKey();
+}
+
+//舵机转向  servo rotate function
+/* 接收到开门/关门信号,舵机旋转
+  1.刷卡
+  2.指纹
+  3.键盘密码输入
+*/
+int servo_turn(int pos, Servo myservo, int lockStatus)
+{
+    if (lockStatus == CLOSED)
+    {
+        for (pos; pos <= 180; pos++)
+        {
+            myservo.write(pos);
+            delay(1);
+        }
+        lockStatus = OPENED;
+    }
+    else
+    {
+        for (pos; pos >= 0; pos--)
+        {
+            myservo.write(pos);
+            delay(1);
+        }
+        lockStatus = CLOSED;
+    }
+    return lockStatus;
+}
+
+//蜂鸣器模块
+void buzzerSpeaker(long noteDuration, int BuzzerPin, bool num)
+{
+    int melody[]{
+        NOTE_G4, NOTE_A4, 0};
+    int noteLength = 1000 / noteDuration;
+    if (num)
+    {
+        for (int i = 0; i < sizeof(melody) / sizeof(int); i++)
+        {
+            tone(BuzzerPin, melody[i], noteLength);
+            delay(noteLength);
+            noTone(BuzzerPin);
+        }
+    }
+    else
+    {
+        tone(BuzzerPin, melody[0], noteLength);
+        delay(noteLength);
+        noTone(BuzzerPin);
+    }
+}
+
+// LCD显示模块 display module
+/*
+  默认显示Enter the Password:
+  1.键盘输入时显示密码输入界面***
+*/
+
+
+
+//密码错误LCD显示
+
+
+// 正确密码显示
+
+
+//键盘输入字符与预设密码比较
+bool passwordCompare(char inputkey, char *password, int place)
+{
+    char *pass_p = password + place;
+    if (*pass_p != inputkey)
+    {
+        return false;
+    }
+    return true;
+}
+
+// 密码错误警报
+void passwordWrong(int RGB_pins[3], int noteDuration, int BuzzerPin)
+{
+
+    RGB_color_red(RGB_pins);
+    buzzerSpeaker(noteDuration, BuzzerPin, WRONG);
+
+    RGB_Init(RGB_pins);
+}
+
+// 密码正确
+void passwordRight( int RGB_pins[3], int noteDuration, int BuzzerPin)
+{
+
+    RGB_color_green(RGB_pins);
+    buzzerSpeaker(noteDuration, BuzzerPin, PASS);
+    delay(5000);
+    RGB_Init(RGB_pins);
+ 
+}
+
+//字符串长度计算
+int _strlen(char *str)
+{
+    char *ptr = str;
+    int len = 0;
+    while (*ptr != NULL)
+    {
+        len++;
+    }
+    return len;
+}
+
+//输入密码
+
+char * enterPassword(Keypad keypad,int *clock)//Modified by JW
+{
+	char *pw_pointer;
+    static char buff[]="!!!!";
+    char* buff_pointer = buff;
+    static char pw[5]="!!!!!";
+    pw_pointer = pw;
+    int i = 0;
+    char key;
+
+    while (((key = keyRead(keypad))) != '*'    )//�������� i<6��ԭ�򣺽���ģʽ�����1��1����������4��������*Ҳ��һ��
+    {
+
+        if((*(buff_pointer+i-1)!=key || i==0 )&& key){
+          *(buff_pointer+i)= key; 
+          Serial.print("Entered key is:");
+          Serial.println(*(buff_pointer+i));    
+          i++;
+          
+        }
+    }
+    if((key = keyRead(keypad))=='*' )//����������ֹ
+    {
+        Serial.println("* is be entered!");
+        *clock=0;
+        pw_pointer=buff_pointer;
+    }
+    for(int ii=0;ii<5;ii++){
+      Serial.println();
+      Serial.print("Entered ps in the headfile is:");
+      Serial.print(*(pw_pointer+ii));
+    }
+    return pw_pointer;
+}
+
+//输入成功后密码比较
+bool inputPasswordCompared(char *input, char *password)
+{
+    bool result = false;
+    if (!_strlen(input) == _strlen(password))
+    {
+        return result;
+    }
+    else
+    {
+        while (*input != NULL && *password != NULL)
+        {
+            if (*input == NULL || *password == NULL || *input != *password)
+            {
+                result = false;
+                return result;
+            }
+            result = true;
+            input++;
+            password++;
+        }
+        return result;
+    }
+}
+
+// 输入模式选择
+int modeSelect(Keypad keypad)
+{
+    return keypad.getKey() - '0';
+}
+
+// 指纹识别初始化
+void fingerPrint_Init(Adafruit_Fingerprint finger)
+{
+    finger.begin(57600);
+    if (finger.verifyPassword())
+    {
+        Serial.println("Found fingerprint sensor!");
+    }
+    else
+    {
+        Serial.println("Did not find fingerprint sensor :(");
+        return;
+    }
+
+    delay(1);
+}
+
+// 指纹ID 选择
+uint8_t readnumber(Keypad keypad)
+{
+    uint8_t num = 0;
+
+    while (num == 0)
+    {
+        num = keyRead(keypad) - '0';
+    }
+    return num;
+}
+
+// Enroll fingerprint
+uint8_t getFingerprintEnroll(Adafruit_Fingerprint finger, uint8_t id, int RGB_pins[3])
+{
+
+    int p = -1;
+    Serial.print("Waiting for valid finger to enroll as #");
+    Serial.println(id);
+    while (p != FINGERPRINT_OK)
+    {
+        p = finger.getImage();
+        switch (p)
+        {
+        case FINGERPRINT_OK:
+            Serial.println("Image taken");
+            break;
+        case FINGERPRINT_NOFINGER:
+            Serial.println(".");
+            break;
+        case FINGERPRINT_PACKETRECIEVEERR:
+            Serial.println("Communication error");
+            break;
+        case FINGERPRINT_IMAGEFAIL:
+            Serial.println("Imaging error");
+            break;
+        default:
+            Serial.println("Unknown error");
+            break;
+        }
+    }
+
+    // OK success!
+
+    p = finger.image2Tz(1);
+    switch (p)
+    {
+    case FINGERPRINT_OK:
+        Serial.println("Image converted");
+        break;
+    case FINGERPRINT_IMAGEMESS:
+        Serial.println("Image too messy");
+        return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+        Serial.println("Communication error");
+        return p;
+    case FINGERPRINT_FEATUREFAIL:
+        Serial.println("Could not find fingerprint features");
+        return p;
+    case FINGERPRINT_INVALIDIMAGE:
+        Serial.println("Could not find fingerprint features");
+        return p;
+    default:
+        Serial.println("Unknown error");
+        return p;
+    }
+
+    Serial.println("Remove finger");
+    delay(2000);
+    p = 0;
+    while (p != FINGERPRINT_NOFINGER)
+    {
+        p = finger.getImage();
+    }
+    Serial.print("ID ");
+    Serial.println(id);
+    p = -1;
+    Serial.println("Place same finger again");
+    while (p != FINGERPRINT_OK)
+    {
+        p = finger.getImage();
+        switch (p)
+        {
+        case FINGERPRINT_OK:
+            Serial.println("Image taken");
+            break;
+        case FINGERPRINT_NOFINGER:
+            Serial.print(".");
+            break;
+        case FINGERPRINT_PACKETRECIEVEERR:
+            Serial.println("Communication error");
+            break;
+        case FINGERPRINT_IMAGEFAIL:
+            Serial.println("Imaging error");
+            break;
+        default:
+            Serial.println("Unknown error");
+            break;
+        }
+    }
+
+    // OK success!
+
+    p = finger.image2Tz(2);
+    switch (p)
+    {
+    case FINGERPRINT_OK:
+        Serial.println("Image converted");
+        break;
+    case FINGERPRINT_IMAGEMESS:
+        Serial.println("Image too messy");
+        return p;
+    case FINGERPRINT_PACKETRECIEVEERR:
+        Serial.println("Communication error");
+        return p;
+    case FINGERPRINT_FEATUREFAIL:
+        Serial.println("Could not find fingerprint features");
+        return p;
+    case FINGERPRINT_INVALIDIMAGE:
+        Serial.println("Could not find fingerprint features");
+        return p;
+    default:
+        Serial.println("Unknown error");
+        return p;
+    }
+
+    // OK converted!
+    Serial.print("Creating model for #");
+    Serial.println(id);
+
+    p = finger.createModel();
+    if (p == FINGERPRINT_OK)
+    {
+        Serial.println("Prints matched!");
+    }
+    else if (p == FINGERPRINT_PACKETRECIEVEERR)
+    {
+        Serial.println("Communication error");
+        return p;
+    }
+    else if (p == FINGERPRINT_ENROLLMISMATCH)
+    {
+        Serial.println("Fingerprints did not match");
+        return p;
+    }
+    else
+    {
+        Serial.println("Unknown error");
+        return p;
+    }
+
+    Serial.print("ID ");
+    Serial.println(id);
+    p = finger.storeModel(id);
+    if (p == FINGERPRINT_OK)
+    {
+        Serial.println("Stored!");
+        RGB_color_blue(RGB_pins);
+        delay(2000);
+        RGB_Init(RGB_pins);
+    }
+    else if (p == FINGERPRINT_PACKETRECIEVEERR)
+    {
+        Serial.println("Communication error");
+        return p;
+    }
+    else if (p == FINGERPRINT_BADLOCATION)
+    {
+        Serial.println("Could not store in that location");
+        return p;
+    }
+    else if (p == FINGERPRINT_FLASHERR)
+    {
+        Serial.println("Error writing to flash");
+        return p;
+    }
+    else
+    {
+        Serial.println("Unknown error");
+        return p;
+    }
+
+    return true;
+}
+
+// finger Detect function
+int getFingerprintIDez(Adafruit_Fingerprint finger, int RGB_pins[3])
+{
+    uint8_t p = finger.getImage();
+    while ((p = finger.getImage()) != FINGERPRINT_OK);
+
+    p = finger.image2Tz();
+    if (p != FINGERPRINT_OK)
+        return -1;
+
+    p = finger.fingerFastSearch();
+    if (p != FINGERPRINT_OK)
+        return -1;
+
+    if (finger.fingerID == 1)
+    {
+        RGB_color_green(RGB_pins);
+        delay(2000);
+        RGB_Init(RGB_pins);
+    }
+
+    // found a match!
+    return finger.fingerID;
+}
